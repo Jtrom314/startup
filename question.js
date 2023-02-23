f63d42f8705560b17d91809701e3f2c315ef1778
Devin <ds698@byu.edu>
2023-02-22 18:09:34 -0700

    fillIn(question.js)

--- question.js
+++ question.js
@@ -1,15 +1,26 @@
 let activeQuestion = 1;
 
+document.getElementById(`prompt${activeQuestion}`).style.display = "block";
+/*"block" means show the block*/
+document.getElementById().style.display = "none";
+/*"none" means hide the block*/
 
-function hideChoicesAndShowResults() {
-
+function hideChoicesAndShowResults(questionNum) {
+  document.getElementById(`prompt${questionNum}`).style.display = "block";
+  document.getElementById(`choices${questionNum}`).style.display = "none";
+  document.getElementById(`results${questionNum}`).style.display = "block";
 }
 
-function showChicesAndHideResults() {
-
+function showChicesAndHideResults(questionNum) {
+  document.getElementById(`prompt${questionNum}`).style.display = "block";
+  document.getElementById(`choices${questionNum}`).style.display = "block";
+  document.getElementById(`results${questionNum}`).style.display = "none";
 }
 
 function nextQuestion() {
-    activeQuestion += 1;
-    
+  activeQuestion += 1;
+}
+
+function showQuestion(questionNumber) {
+  activeQuestion = questionNumber;
 }
