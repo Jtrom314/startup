49ba372baf73ac0ddec9f2ce8d59ef49b0ba8ee5
Jtrom314 <jtrom2021@gmail.com>
2023-02-22 17:51:18 -0700

    started outline for questions.js

---
+++ question.js
@@ -0,0 +1,15 @@
+let activeQuestion = 1;
+
+
+function hideChoicesAndShowResults() {
+
+}
+
+function showChicesAndHideResults() {
+
+}
+
+function nextQuestion() {
+    activeQuestion += 1;
+    
+}
