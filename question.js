5f354c2d6a5569fa2455ba032cac3482405b254e
Devin <ds698@byu.edu>
2023-02-22 18:34:43 -0700

    add(nextButton functionality)

--- question.js
+++ question.js
@@ -1,26 +1,41 @@
+
 let activeQuestion = 1;
 
-document.getElementById(`prompt${activeQuestion}`).style.display = "block";
+document.getElementById(`question${activeQuestion}Wrapper`).style.display = "block";
 /*"block" means show the block*/
 document.getElementById().style.display = "none";
 /*"none" means hide the block*/
 
-function hideChoicesAndShowResults(questionNum) {
-  document.getElementById(`prompt${questionNum}`).style.display = "block";
-  document.getElementById(`choices${questionNum}`).style.display = "none";
-  document.getElementById(`results${questionNum}`).style.display = "block";
+function onLoad() {
+  let prompts = document.getElementsByClassName(`questionWrap`);
+  //get all the elements with class name "questionWrap"
+  for (let i = 0; i < prompts.length; i++) {
+    if (i != activeQuestion - 1) {
+      prompts[i].style.display = "none";
+    } else {
+      prompts[i].style.display = "block";
+    }
+  }
 }
 
-function showChicesAndHideResults(questionNum) {
-  document.getElementById(`prompt${questionNum}`).style.display = "block";
-  document.getElementById(`choices${questionNum}`).style.display = "block";
-  document.getElementById(`results${questionNum}`).style.display = "none";
+function hideChoicesAndShowResults() {
+  document.getElementById(`question${activeQuestion}`).style.display = "block";
+  document.getElementById(`choices${activeQuestion}`).style.display = "none";
+  document.getElementById(`results${activeQuestion}`).style.display = "block";
 }
 
-function nextQuestion() {
-  activeQuestion += 1;
+function showChicesAndHideResults() {
+  document.getElementById(`question${activeQuestion}`).style.display = "block";
+  document.getElementById(`choices${activeQuestion}`).style.display = "block";
+  document.getElementById(`results${activeQuestion}`).style.display = "none";
 }
 
 function showQuestion(questionNumber) {
   activeQuestion = questionNumber;
+  onload();
+}
+
+function showNextQuestion() {
+  activeQuestion += 1;
+  onload();
 }
