8918f1ad11f4d3a53639dd6255c10f5c5a798192
Jtrom314 <jtrom2021@gmail.com>
2023-03-13 18:12:20 -0600

    Bug fixed login

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -9,10 +11,37 @@ if (!userName) {
 }
 
 const url = `mongodb+srv://${userName}:${password}@${hostname}.yrwizyx.mongodb.net/bnb`;
-console.log(url);
+
 
 const client = new MongoClient(url);
 const responseCollection = client.db('startup').collection('responses');
+const userCollection = client.db('startup').collection('user');
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    }
+    await userCollection.insertOne(user);
+    return user;
+}
+
+function returnAllUsers() {
+    const query = {};
+    const options ={};
+    const all = userCollection.find(query, options);
+    return all.toArray();
+}
 
 function addResponse(response) {
     responseCollection.insertOne(response);
@@ -25,4 +54,11 @@ function returnResponses() {
     return all.toArray();
 }
 
-module.exports = { addResponse, returnResponses };
+module.exports = { 
+    addResponse, 
+    returnResponses,
+    getUser,
+    createUser,
+    getUserByToken, 
+    returnAllUsers
+};
