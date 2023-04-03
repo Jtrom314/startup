557b8c7a7d9e31908f2238f3817f12f00e8bb914
Jtrom314 <jtrom2021@gmail.com>
2023-04-03 11:51:44 -0600

    Started working on App funciton. Inserted Header and Footer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <span className="floatLeft">
+        <h1>Y/N.io</h1>
+      </span>
+      <span id="logoDiv">
+        <img
+          src="https://play-lh.googleusercontent.com/HnrIhQ3OLrboERhy6D-olqjQpZQLJYyW9Qz4MXvFcr87qGh91r5gJas2OuoYGJjmA26L=w240-h480-rw"
+          width="75"
+          alt="Yes or No logo"
+        />
+      </span>
       </header>
+
+      <footer>
+      <h5>Created by Jacob Trader</h5>
+      <h5><a href="https://github.com/Jtrom314/startup#startup">Github</a></h5>
+      </footer>
     </div>
   );
 }
